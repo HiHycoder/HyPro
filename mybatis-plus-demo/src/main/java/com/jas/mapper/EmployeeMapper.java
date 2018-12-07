d5624fe33bec30dfdb325d9b58742d9ba54f5266
HiHycoder <hanyihihy@163.com>
2018-12-07 13:58:39 +0800

    aaa

---
+++ mybatis-plus-demo/src/main/java/com/jas/mapper/EmployeeMapper.java
@@ -0,0 +1,9 @@
+package com.jas.mapper;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.jas.bean.Employee;
+
+/**
+ * 不定义任何接口方法
+ */
+public interface EmployeeMapper extends BaseMapper<Employee> {}
